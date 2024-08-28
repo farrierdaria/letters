07ae78e5b8928a1a66d4472968f933397f935e45
farrierdaria <dk18525384@gmail.com>
2024-08-28 09:41:52 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+let burgerMenu = document.querySelector('.header__nav-burger');
+let navMenu = document.querySelector('.menu-container');
+let navContainer = document.querySelector('.header__nav');
+
+function BurgerClicked() {
+    if (burgerMenu.classList.contains('active')) {
+        burgerMenu.classList.remove('active');
+        navMenu.classList.remove('active');
+        navContainer.classList.remove('active');
+    } else {
+        burgerMenu.classList.add('active');
+        navMenu.classList.add('active');
+        navContainer.classList.add('active');
+    }
+}
